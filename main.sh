ef4e91187ae0b236f0b459cf7b02c0638714dcd5
fangyihaoo <fangyihao116@gmail.com>
2023-04-11 13:54:52 -0400

    Update main.sh
    Update commands for mean curvature and Willmore flow

--- main.sh
+++ main.sh
@@ -1,12 +1,13 @@
 #!/bin/csh
-#$ -M zhu4@nd.edu     # Email address for job notification
+#$ -M yfang5@nd.edu     # Email address for job notification
 #$ -m abe               # Send mail when job begins, ends and aborts
 #$ -q gpu             
 #$ -l gpu_card=1
-#$ -N EVNNToyPoiCycleDritz
+#$ -N circleLamda10
 
-module load python
-
+echo "This job is running on the host $HOSTNAME"
+echo "This job was assigned GPU: $CUDA_VISIBLE_DEVICES"
+module load python/3.7.3
 
 # python ./Tasks/toyEVNN.py train --type='poissoncycle' --exact=poiss2dcycleexact.pt --grid=poiss2dcyclegrid.pt
 
@@ -22,6 +23,20 @@ module load python
 
 # python ./Tasks/Heat.py train 
 
-python ./Tasks/AllenCahn.py train 
+#python3 ./Tasks/AllenCahn.py train
 
-# python ./Tasks/Fokker2d.py train
+#python3 ./Tasks/MeanCur.py train --max_epoch=1000 --pretrain="ellipseInitilizationLFBGS.pt"
+
+#python3 ./Tasks/MeanCur.py train --max_epoch=1000 --pretrain="dumbbellInitilizationLFBGS.pt"
+
+#python3 ./Tasks/MeanCur.py train --max_epoch=1000 --pretrain="squareInitilizationLFBGS.pt"
+
+python3 ./Tasks/MeanCur.py train --max_epoch=2000 --pretrain="circleInitilizationLFBGS.pt"
+
+#python3 ./Tasks/MeanCur.py train --max_epoch=1000 --pretrain="twoEllipseInitilizationLFBGS.pt"
+
+#python ./Tasks/Fokker2d.py train
+
+#python3 utils/pretrain.py pretrain_Mean3d --max_epoch=20000 --lr_decay=0.7 --step_size=5000 --lr=1e-2
+
+#python3 ./Tasks/Willmore3d.py train --max_epoch=200
