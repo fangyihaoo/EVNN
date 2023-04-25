28487822bff959f852a77fbb1193eed40cb682cb
fangyihaoo <fangyihao116@gmail.com>
2023-04-25 13:29:21 -0400

    Update __init__.py

--- Model/__init__.py
+++ Model/__init__.py
@@ -1,2 +1,2 @@
 from .network import FullNet, ResidualNet, NormalFlow
-from .setting import AllenCahnConfig, FokkerConfig, HeatConfig, PoissonConfig, PorusConfig, PoissonDritzConfig, HighDimConfig
+from .setting import Willmore3dConfig,MeanCurConfig, AllenCahnConfig, FokkerConfig, HeatConfig, PoissonConfig, PorusConfig, PoissonDritzConfig, HighDimConfig
