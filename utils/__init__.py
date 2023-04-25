1cffc14d5629a494ffd8f0d5d4a4a70d3ea2cb58
fangyihaoo <fangyihao116@gmail.com>
2023-04-25 13:26:36 -0400

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from .log_helper import logger_init
-from .gen_sample import poisson, allencahn, heatpinn, heat, poissoncycle, PoiHighGrid, disk_grid_regular
-from .lossfunc import PoiLoss, PoiHighLoss, AllenCahnW, HeatPINN, PoissPINN, PoissCyclePINN, PoiCycleLoss, Heat, FokkerPlanck
+from .gen_sample import meancur,poisson, allencahn, heatpinn, heat, poissoncycle, PoiHighGrid, disk_grid_regular
+from .lossfunc import Willmore3dLoss,WillmoreLoss,MeanCurLoss,PoiLoss, PoiHighLoss, AllenCahnW, HeatPINN, PoissPINN, PoissCyclePINN, PoiCycleLoss, Heat, FokkerPlanck
 from .gen_exact import PoiHighExact
 from .parameter import count_parameters, weight_init
 from .helper import V, MulNormal
